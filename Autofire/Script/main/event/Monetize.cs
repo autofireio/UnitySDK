101eb37133efc4ce6bb90364632f72794a2fcd19
Dionysios G. Kakolyris <dio@DGKs-MacBook-Pro>
2018-09-17 16:24:37 +0300

    Monetize event parameters

--- Autofire/Script/main/event/Monetize.cs
+++ Autofire/Script/main/event/Monetize.cs
@@ -6,24 +6,24 @@
 
 		public const string MONETIZE_NAME = "MONETIZE";
 
-		private string item;
+		private string monetizeName;
 		private int ac;
 		private int qty;
 
-		public Monetize (string item, int ac, int qty) :
+		public Monetize (string name, int ac, int qty) :
 			base (MONETIZE_NAME)
 		{
-			this.item = item;
+			this.monetizeName = name;
 			this.ac = ac;
 			this.qty = qty;
-			WithPredefinedFeature ("name", item);
+			WithPredefinedFeature ("name", name);
 			WithPredefinedFeature ("ac", ac);
 			WithPredefinedFeature ("qty", qty);
 		}
 
-		public string GetItem ()
+		public string GetName ()
 		{
-			return item;
+			return monetizeName;
 		}
 
 		public int GetAc ()
